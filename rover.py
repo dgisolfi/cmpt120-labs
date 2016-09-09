d1fddc8306b65694473709491f55a43ebc462b1b
Daniel Gisolfi <daniel.gisolfi1@marist.edu>
2016-09-09 11:57:53 -0400

    solution to rover excersise.

---
+++ rover.py
@@ -0,0 +1,14 @@
+#Into to programing
+#Author: Daniel GIsolfi
+#Date: 9/9/16
+#rover.py
+
+def main():
+	
+	distance = 34000000
+	speed = 186000
+	time = distance/speed
+
+	print("it takes" ,time,"seconds to reach nasa")
+
+main()
