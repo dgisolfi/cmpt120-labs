fde2095db36f6bdbfc9ae391c86dbd65ce8ce37e
Daniel Gisolfi <daniel.gisolfi1@marist.edu>
2016-09-09 11:48:58 -0400

    solution to mad libs excersise.

---
+++ madlib.py
@@ -0,0 +1,14 @@
+#Into to programing
+#Author: Daniel GIsolfi
+#Date: 9/9/16
+#madlib.py
+
+def main():
+	noun = input("Enter a noun:")
+	verb = input("Enter a verb:")
+	adjective = input("Enter a adjective:")
+	place = input("Enter a place:")
+
+	sentence = noun + adjective + verb + place
+	
+main()
