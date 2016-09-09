22e8fa3d8aa4575693c91f28d7508f7d5632b727
Daniel Gisolfi <daniel.gisolfi1@marist.edu>
2016-09-09 11:38:26 -0400

    solutions to evens loop excersie.

--- evens.py
+++ evens.py
@@ -1,2 +1,10 @@
-for i in range(10):
-	print(i * 2)
+#Into to programing
+#Author: Daniel GIsolfi
+#Date: 9/9/16
+#evens.py
+
+def main():
+    for i in range(1,11):
+            print(i * 2)
+
+main()
