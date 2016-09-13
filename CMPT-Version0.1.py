d3e29725bbe39d7e57f9f46f7cc164995fbc96d7
Daniel Gisolfi <daniel.gisolfi1@marist.edu>
2016-09-13 11:55:01 -0400

    my project version 0.1

---
+++ CMPT-Version0.1.py
@@ -0,0 +1,36 @@
+#Into to programing
+#Author: Daniel Gisolfi
+#Date: 9/13/16
+#CMPT-Version0.1
+
+title = "The Abandoned Lab"
+
+#Fixed Locations
+labentrance = "You are at the Lab entrance"
+computerlab = "You are in the Computer Lab"
+datacenter =  "You are in the Data Center"
+servicedesk =  "You are at the Service Desk"
+
+#player location
+playerlocation = labentrance
+
+
+print(title)
+print(playerlocation)
+input("Press Enter to continue")
+
+#location randomizer
+# import random
+# locationvalue = random.random([1,4])
+
+# if locationvalue == playerlocation:
+# 	locationvalue = random.random([1,4])
+# else: 
+# 	print(playerlocation) 
+
+playerlocation = datacenter
+
+
+
+
+
