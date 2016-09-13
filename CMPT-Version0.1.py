23d02f342f920809eb1fff9aa987f43d88cd3e26
Daniel Gisolfi <daniel.gisolfi1@marist.edu>
2016-09-13 19:41:29 -0400

    my project version 0.1

--- CMPT-Version0.1.py
+++ CMPT-Version0.1.py
@@ -3,34 +3,42 @@
 #Date: 9/13/16
 #CMPT-Version0.1
 
-title = "The Abandoned Lab"
+def main():
+#Start Game
+    title = "The Abandoned Lab:"
+    backstory =  "You have arrived at a large metal door leading to an abandoned computer labratory, explore inside to discover new technology"
+    labentrance = "You are at the Lab entrance"
+    computerlab = "You are in the Computer Lab"
+    datacenter =  "You are in the Data Center"
+    servicedesk =  "You are at the Service Desk"
+    score = int(0)
+    playerlocation = labentrance
+    print(title)
+    print(backstory)
+    print(playerlocation)
+    input("Press Enter to continue")
 
-#Fixed Locations
-labentrance = "You are at the Lab entrance"
-computerlab = "You are in the Computer Lab"
-datacenter =  "You are in the Data Center"
-servicedesk =  "You are at the Service Desk"
+#Game
+    x = int(0)
+    while x < 3:
+        if x == 0:
+            playerlocation = computerlab
+        elif x == 1:
+            playerlocation = servicedesk
+        elif x == 2:
+            playerlocation = datacenter
+            
+        score = score + 5
+        print(playerlocation,"Your score is",score)
+        input("Press Enter to continue")
+        x = x + 1
 
-#player location
-playerlocation = labentrance
+#End Game
+    conclusion = "Congradulations, you found the data center and new technology inside"
+    copyright = "Name: Daniel Gisolfi Email: Daniel.Gisolfi1@marist.edu"
+    print(conclusion)
+    print(copyright)
 
+main()
 
-print(title)
-print(playerlocation)
-input("Press Enter to continue")
-
-#location randomizer
-# import random
-# locationvalue = random.random([1,4])
-
-# if locationvalue == playerlocation:
-# 	locationvalue = random.random([1,4])
-# else: 
-# 	print(playerlocation) 
-
-playerlocation = datacenter
-
-
-
-
-
+	
