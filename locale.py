098ae45cb07f712fe56738c0956126a4f366c28e
Daniel Gisolfi <daniel.gisolfi1@marist.edu>
2016-11-11 11:37:17 -0500

    finished locale.py

---
+++ locale.py
@@ -0,0 +1,25 @@
+#Into to programing
+#Author: Daniel Gisolfi
+#Date: 11/11/16
+#locale.py
+
+class locale:
+	#constructor
+	def __init__(self, name, description, item):
+		self.name = name	#atrributes of player
+		self.description = description
+		self.isVisited = False
+		self.item = item
+
+	def render(self, description):
+		if isVisited == True:
+			self.description == "You have returned to "
+			print(self.description, self.name)
+
+	def removeItem(self, item):
+		if self.item == item:
+			self.item = None
+	def visit():
+		self.isVisited = True
+
+
